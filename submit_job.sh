4a36291cbf2df7270a0b6d15052eadf503932580
Julian Klüber <julian.klueber@uni-jena.de>
2024-08-26 15:42:20 +0200

    wrote slurm script

---
+++ submit_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=toxicity_analysis 
+#SBATCH --output=log-%j.log              
+#SBATCH --error=error-%j.log             
+#SBATCH --gres=gpu:ampere:1              
+#SBATCH --mem=32G                        
+#SBATCH --cpus-per-task=4                
+#SBATCH --time=2-00:00:00                 
+#SBATCH --container-image=pytorch/pytorch:2.1.1-cuda12.1-cudnn8-runtime
+
+srun python3 toxic-bert/src/main.py
