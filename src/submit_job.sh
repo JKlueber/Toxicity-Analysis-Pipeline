a764bd42517d643998dc83ece656d08debc5b0c5
Julian Klüber <julian.klueber@uni-jena.de>
2024-09-11 14:27:44 +0200

    updated Docker

--- src/submit_job.sh
+++ src/submit_job.sh
@@ -6,11 +6,11 @@
 #SBATCH --mem=64G                        
 #SBATCH --cpus-per-task=4                
 #SBATCH --time=2-00:00:00                 
-#SBATCH --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.2
+#SBATCH --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.3
 #SBATCH --container-mounts=/etc/slurm:/etc/slurm,/usr/lib/x86_64-linux-gnu/slurm:/usr/lib/x86_64-linux-gnu/slurm,/run/munge:/run/munge
 
 # Debug the environment
-srun --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.2 \
+srun --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.3 \
      python3 -c "import torch; print(torch.cuda.is_available())"
 
 srun python3 toxic-bert/main.py
