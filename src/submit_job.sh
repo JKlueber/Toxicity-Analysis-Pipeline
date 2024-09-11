9169b558a4a96ea310a022e353afb0ca3b59c513
Julian Klüber <julian.klueber@uni-jena.de>
2024-09-11 13:24:55 +0200

    updated slurm script

--- src/submit_job.sh
+++ src/submit_job.sh
@@ -3,9 +3,13 @@
 #SBATCH --output=../data/output/log-%j.log              
 #SBATCH --error=../data/output/error-%j.log             
 #SBATCH --gres=gpu:ampere:1              
-#SBATCH --mem=32G                        
+#SBATCH --mem=64G                        
 #SBATCH --cpus-per-task=4                
 #SBATCH --time=2-00:00:00                 
-#SBATCH --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.1
+#SBATCH --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.2
+
+# Debug the environment
+srun --container-image=registry.webis.de/code-teaching/theses/thesis-klueber/toxicity:0.0.2 \
+     python3 -c "import torch; print(torch.cuda.is_available())"
 
 srun python3 toxic-bert/main.py
