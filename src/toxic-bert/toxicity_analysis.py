ed4083f5635ad21f5bcea5d9e9a3947ed4c608b7
Julian Klüber <julian.klueber@uni-jena.de>
2024-09-17 20:26:58 +0200

    updated path

--- src/toxic-bert/toxicity_analysis.py
+++ src/toxic-bert/toxicity_analysis.py
@@ -68,7 +68,7 @@ def load_toxicity_model():
         return_all_scores=True
     )
 
-def measure_toxicity(filtered_search, es, index, lang_detector, toxic_bert, batch_size=128, output_path='../data/output/toxicity_results.json'):
+def measure_toxicity(filtered_search, es, index, lang_detector, toxic_bert, batch_size=128, output_path='../../data/output/toxicity_results.json'):
     toxicitys = []
     batch = []
 
@@ -103,7 +103,7 @@ def measure_toxicity(filtered_search, es, index, lang_detector, toxic_bert, batc
                         'toxicity': toxicity.to_dict()
                     })
                 batch = []
-        if count > 2:
+        if count > 5:
             break
 
     if batch:
