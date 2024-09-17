13ceb7e80d49f67aa563f3b7b8c27646d4fb40bb
Julian Klüber <julian.klueber@uni-jena.de>
2024-09-17 20:27:21 +0200

    removed csv storing

--- src/toxic-bert/main.py
+++ src/toxic-bert/main.py
@@ -21,23 +21,13 @@ def main():
     filtered_search = base_search.filter(Term(language=language))
 
     lang_detector = load_language_detector()
-    toxic_bert = load_toxicity_model().to('cuda')
+    toxic_bert = load_toxicity_model()#.to('cuda')
 
     batch_size = config['toxicity_analysis']['batch_size']
     index = config['elasticsearch']['index']
 
     toxicitys = measure_toxicity(filtered_search, es, index, lang_detector, toxic_bert, batch_size=2)
     print(toxicitys)
-
-    # output_path = Path(f"../data/toxicity_results_{os.getenv('SLURM_ARRAY_TASK_ID', '0')}.txt")
-    # with output_path.open("w", newline='') as csvfile:
-    #     fieldnames = ['instance', 'toxicity', 'severe_toxicity', 'obscene', 'threat', 'insult', 'identity_attack']
-    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
-    #     writer.writeheader()
-    #     for result in results:
-    #         writer.writerow(result)
-
 
 if __name__ == "__main__":
     main()
