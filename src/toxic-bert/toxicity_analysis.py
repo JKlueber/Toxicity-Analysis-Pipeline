9c66bceb389205811910707027d4538f9d35ad73
Julian Klüber <julian.klueber@uni-jena.de>
2024-09-18 18:07:18 +0200

    debugged

--- src/toxic-bert/toxicity_analysis.py
+++ src/toxic-bert/toxicity_analysis.py
@@ -66,7 +66,7 @@ def load_toxicity_model():
         tokenizer='bert-base-uncased', 
         function_to_apply='sigmoid', 
         return_all_scores=True
-    )
+    ).to('cuda')
 
 def measure_toxicity(filtered_search, es, index, lang_detector, toxic_bert, batch_size=128, output_path='data/output/toxicity_results.json'):
     toxicitys = []
