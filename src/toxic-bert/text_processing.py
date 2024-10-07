1e516c48d20d3d521bfa5ea226e387d6b69e2baf
Julian Klüber <julian.klueber@uni-jena.de>
2024-10-07 20:58:06 +0200

    debuged

--- src/toxic-bert/text_processing.py
+++ src/toxic-bert/text_processing.py
@@ -2,10 +2,12 @@ from resiliparse.extract.html2text import extract_plain_text
 import fasttext
 from huggingface_hub import hf_hub_download
 from pathlib import Path
-
 from nltk.corpus import stopwords
+import nltk
 
-stopwords = set(stopwords.words('english'))
+nltk.download('stopwords')
+
+english_stopwords = set(stopwords.words('english'))
 
 def load_language_detector():
     model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
@@ -20,7 +22,7 @@ def extract_text_from_hit(hit):
     )
     
     filtered_words = [
-        word for word in text.split() if word.lower() not in stopwords
+        word for word in text.split() if word.lower() not in english_stopwords
     ]
     
     filtered_text = ' '.join(filtered_words)
