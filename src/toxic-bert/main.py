f307ac3c7898fd578573c965df473159f0503ffb
Julian Klüber <julian.klueber@uni-jena.de>
2024-10-14 19:54:40 +0200

    added Debug

--- src/toxic-bert/main.py
+++ src/toxic-bert/main.py
@@ -5,28 +5,27 @@ from text_processing import load_language_detector
 from toxicity_analysis import load_toxicity_model, ToxicityClassifier
 
 from ray import init
-from ray.data import read_datasource
+from ray.data import read_datasource, DataContext
 import time
 
 
 init()
+DataContext.get_current().enable_operator_progress_bars = True
 
 def main():
     config_path = Path("data/config/config.yaml")
     config = load_config(config_path)
 
-    lang_detector = load_language_detector()
-    toxic_bert = load_toxicity_model()
-
     batch_size = config['toxicity_analysis']['batch_size']
 
     es_source = get_es_source(config)
 
+    print("Reading data from Elasticsearch...")
     start_time = time.time()
     read_datasource(es_source) \
     .map_batches(
-        ToxicityClassifier(language_detector=lang_detector, classifier=toxic_bert),
-        concurrency=4,
+        ToxicityClassifier(),
+        concurrency=100,
         num_gpus=0,  
         batch_size=batch_size) \
     .write_json("local:///mnt/ceph/storage/data-tmp/2024/po87xox/toxicity")
