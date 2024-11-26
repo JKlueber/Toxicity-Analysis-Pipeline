cac0bb1123a77778a705e676a559294adce7474f
Julian Klüber <julian.klueber@uni-jena.de>
2024-11-26 10:46:07 +0100

    deleted unused

--- src/toxic_bert/language.py
+++
@@ -1,8 +0,0 @@
-from pandas import DataFrame
-
-def detect_language(batch: DataFrame) -> DataFrame:
-    raise NotImplementedError()
-
-
-def is_english(row) -> bool:
-    raise NotImplementedError()
