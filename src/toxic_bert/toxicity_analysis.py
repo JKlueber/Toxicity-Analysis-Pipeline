358744a6c6cefaa2bb4994d2a7cd0ba603820219
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-11-29 01:54:13 +0100

    Simplify toxicity classifier

--- src/toxic_bert/toxicity_analysis.py
+++ src/toxic_bert/toxicity_analysis.py
@@ -1,88 +1,51 @@
-from transformers import pipeline
-import torch
+from functools import cached_property
+
+from torch import device
+from torch.cuda import is_available as cuda_is_available
 from pandas import DataFrame
+from transformers import pipeline, Pipeline
 
-class Toxicity:
-    def __init__(self, toxicity=0, severe_toxicity=0, obscene=0, threat=0, insult=0, identity_attack=0):
-        self.toxicity = toxicity
-        self.severe_toxicity = severe_toxicity
-        self.obscene = obscene
-        self.threat = threat
-        self.insult = insult
-        self.identity_attack = identity_attack
-    
-    def __add__(self, other):
-        return Toxicity(
-            self.toxicity + other.toxicity,
-            self.severe_toxicity + other.severe_toxicity,
-            self.obscene + other.obscene,
-            self.threat + other.threat,
-            self.insult + other.insult,
-            self.identity_attack + other.identity_attack
-        )
-    
-    def __truediv__(self, other):
-        return Toxicity(
-            self.toxicity / other,
-            self.severe_toxicity / other,
-            self.obscene / other,
-            self.threat / other,
-            self.insult / other,
-            self.identity_attack / other
-        )
-    
-    def __str__(self):
-        return f"Toxicity: {self.toxicity}, Severe Toxicity: {self.severe_toxicity}, Obscene: {self.obscene}, Threat: {self.threat}, Insult: {self.insult}, Identity Attack: {self.identity_attack}"
-
-    @classmethod
-    def from_prediction(cls, prediction):
-        scores = {item['label']: item['score'] for item in prediction[0]}
-        return cls(
-            toxicity=scores.get('toxic', 0),
-            severe_toxicity=scores.get('severe_toxic', 0),
-            obscene=scores.get('obscene', 0),
-            threat=scores.get('threat', 0),
-            insult=scores.get('insult', 0),
-            identity_attack=scores.get('identity_hate', 0)
-        )
-    
-    def to_dict(self):
-        return {
-            'toxicity': self.toxicity,
-            'severe_toxicity': self.severe_toxicity,
-            'obscene': self.obscene,
-            'threat': self.threat,
-            'insult': self.insult,
-            'identity_attack': self.identity_attack
-        }
-
-def load_toxicity_model():
-    # device = 0 if torch.cuda.is_available() else -1
-
-    return pipeline(
-        'text-classification', 
-        model='unitary/toxic-bert', 
-        tokenizer='bert-base-uncased', 
-        top_k=None,
-        truncation=True,
-        padding=True,
-        # TODO: Use GPU here with `device=` if available.
-    )
 
 class ToxicityClassifier:
-    def __init__(self):
-        self.classifier = None
 
-    def initialize(self):
-        if self.classifier is None:
-            self.classifier = load_toxicity_model()
-    
-    def __call__(self, batch: DataFrame) -> DataFrame:
-        self.initialize()
-
-        batch["toxicity"] = batch["plaintext"].apply(
-            lambda plaintext: Toxicity.from_prediction(self.classifier(plaintext)).to_dict()
+    @cached_property
+    def _pipeline(self) -> Pipeline:
+        return pipeline(
+            "text-classification",
+            model="unitary/toxic-bert",
+            tokenizer="bert-base-uncased",
+            top_k=None,  # all labels
+            truncation=True,
+            padding=True,
+            device=device("cuda" if cuda_is_available() else "cpu"),
         )
 
-        return batch
+    def __call__(self, batch: DataFrame) -> DataFrame:
+        # Reset call count of classifier pipeline.
+        self._pipeline.call_count = 0
 
+        # Predict full batch in one call to the model (pipeline).
+        predictions = self._pipeline(list(batch["plaintext"]))
+
+        # Extract the individual label scores per prediction.
+        scores_per_label = [
+            {item["label"]: item["score"] for item in prediction}
+            for prediction in predictions
+        ]
+
+        # Store each label's scores into separate columns.
+        labels_to_columns = {
+            "toxic": "toxicity",
+            "severe_toxic": "severe_toxicity",
+            "obscene": "obscenity",
+            "threat": "threat",
+            "insult": "insult",
+            "identity_hate": "identity_attack",
+        }
+        for label, column in labels_to_columns.items():
+            batch[column] = [
+                scores.get(label, 0)
+                for scores in scores_per_label
+            ]
+
+        return batch
