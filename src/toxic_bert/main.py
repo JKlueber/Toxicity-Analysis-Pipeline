4f51896d0eec0bde9412810650f7f3f40271dcdb
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-11-29 02:04:44 +0100

    Tune inference pipeline

--- src/toxic_bert/main.py
+++ src/toxic_bert/main.py
@@ -1,16 +1,15 @@
 from pathlib import Path
+
+from ray import init
+from ray.data import read_datasource
+
 from src.toxic_bert.config_loader import load_config
 from src.toxic_bert.elasticsearch_utils import get_es_source
 from src.toxic_bert.toxicity_analysis import ToxicityClassifier
 from src.toxic_bert.text_processing import extract_text
 from src.toxic_bert.language_detection import LanguageDetector
 
-from ray import init
-from ray.data import read_datasource, DataContext
-
 init()
-DataContext.get_current().enable_operator_progress_bars = True
-RAY_ENABLE_RECORD_ACTOR_TASK_LOGGING=1
 
 def main():
     config_path = Path("data/config/config.yaml")
@@ -33,40 +32,41 @@ def main():
         .rename_columns(
             names={
                 "_id": "id",
-                "_source.content": "content",
-                "_source.crawled_from_instance": "crawled_from_instance",
-                "_source.instance": "instance",
-                "_source.is_local": "is_local",
-            }, 
+            },
+            concurrency=500,
             num_cpus=0.01,
         )
         # Map batches to extract text.
         .map_batches(
             fn=extract_text,
-            num_cpus=0.01,
+            concurrency=500,
+            num_cpus=0.05,
             batch_format="pandas",
         )
         # Map batches to add language tag.
         .map_batches(
             LanguageDetector(),
-            concurrency=100,
+            concurrency=500,
             num_cpus=0.25,
             num_gpus=0,
             batch_format="pandas",
         )
         # Filter for English text.
         .filter(
-            lambda batch: batch['language'] == '__label__eng_Latn'
+            lambda batch: batch['language'] == '__label__eng_Latn',
+            concurrency=500,
+            num_cpus=0.01,
         )
         # Classify toxiticity in batches.
         .map_batches(
             ToxicityClassifier(),
-            concurrency=10,
+            concurrency=100,
             num_cpus=0.25,
             num_gpus=0,
-            batch_size=32,
+            # batch_size=32,
             batch_format="pandas",
         )
+        # .limit(1000)
         .write_json(
             path="/mnt/ceph/storage/data-in-progress/data-teaching/theses/thesis-klueber/toxicity",
             concurrency=10,
