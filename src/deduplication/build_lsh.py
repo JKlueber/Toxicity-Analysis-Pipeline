e9693736f5e05e1e3b5517fcc19d6e92ce86c3f9
Julian Klüber <julian.klueber@uni-jena.de>
2025-04-13 09:55:58 +0200

    updated lsh building process

--- src/deduplication/build_lsh.py
+++ src/deduplication/build_lsh.py
@@ -1,78 +1,60 @@
 import os
 from ray import init
-from ray.data import read_datasource
+import ray
+from ray.data import read_parquet_bulk
+from pathlib import Path
 
-from src.utils.elasticsearch_utils import get_es_source_deduplication
 from src.config.config_loader import load_config
-from src.data_processing.text_extractor import extract_text
-from src.utils.minhash_utils import LSHBuilder
-
-import pandas as pd
-from datasketch import MinHashLSH
-
-import pickle
-
-from ray.data.aggregate import AggregateFn
+from src.utils.minhash_utils import LSHBuilder, MergeLSHActor
 
 os.environ["RAY_RUNTIME_ENV_TEMPORARY_REFERENCE_EXPIRATION_S"] = "3600"
 
-init()
+init(ignore_reinit_error=True)
 
 def main():
     config = load_config()
-    output_dir = config["deduplication"]["output_dir"]
-    es_source = get_es_source_deduplication(config)
+    input_dir = config["dir"]["input_sample_dir"]
+    output_dir = config["dir"]["lsh_dir"]
+    output_file_name = "lsh.pkl"
+    file_paths = [str(file) for file in Path(input_dir).glob("*.parquet")]
 
-    lsh_ds = (
-        read_datasource(
-            datasource=es_source,
-            concurrency=1000,
-            override_num_blocks=1000,
+    merge_lsh = MergeLSHActor.remote()
+
+    (
+        read_parquet_bulk(
+            paths=file_paths,
+            concurrency=100,
+            columns=["_id", "minhash"],
             ray_remote_args=dict(
                 num_cpus=0.01,
                 memory=2 * 1024**3,
                 max_retries=10, 
                 retry_exceptions=True,
-            ),
-        )
-        .map_batches(
-            fn=extract_text,
-            concurrency=1000,
-            num_cpus=0.01,
-            batch_format="pandas",
-            memory=2 * 1024**3,
+            )
         )
         .map_batches(
             fn=LSHBuilder(),
-            concurrency=1000,
+            concurrency=100,
             num_cpus=0.01,
             batch_format="pandas",
-            memory=2 * 1024**3,
+            memory=16 * 1024**3,
+            max_retries=10, 
+            retry_exceptions=True,
         ) 
+        .map_batches(
+            lambda batch: ray.get(merge_lsh.merge.remote(batch)),
+            concurrency=100,
+            num_cpus=0.01,
+            num_gpus=0,
+            batch_format="pandas",
+            memory=16 * 1024**3,
+            max_retries=10, 
+            retry_exceptions=True,       
+        )
         .materialize()
     )
 
-    def merge_lsh(lsh1: MinHashLSH, lsh2: MinHashLSH) -> MinHashLSH:
-        lsh1.merge(lsh2)
-        return lsh1
-    
-    def accumulate_lsh(lsh: MinHashLSH, row: pd.DataFrame) -> MinHashLSH:
-        lsh.merge(row["lsh"])
-        return lsh
-    
-    aggregation = AggregateFn(
-        init=lambda _: MinHashLSH(num_perm=128, threshold=0.9),
-        accumulate_row=accumulate_lsh,
-        merge = merge_lsh,
-        name="lsh"
-    )
-
-    lsh_dict = lsh_ds.aggregate(aggregation)
-    file_name = "lsh_test.pkl"
-    lsh = lsh_dict["lsh"]
-
-    with open(output_dir+file_name, "wb") as file:
-        pickle.dump(lsh, file)
+    ray.get(merge_lsh.store_lsh.remote(output_path=output_dir+output_file_name))
 
 if __name__ == "__main__":
     main()
