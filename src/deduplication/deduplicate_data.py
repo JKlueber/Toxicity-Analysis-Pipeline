09a90a5f75b888dd70b12acf19df9ce3a5ed3b3f
Julian Klüber <julian.klueber@uni-jena.de>
2025-04-13 09:56:24 +0200

    updated deduplicate process

--- src/deduplication/deduplicate_data.py
+++ src/deduplication/deduplicate_data.py
@@ -1,52 +1,52 @@
 import os
 from ray import init
-from ray.data import read_datasource
+from ray.data import read_parquet_bulk
 
-from src.utils.elasticsearch_utils import get_es_source_deduplication
+from pathlib import Path
+
 from src.config.config_loader import load_config
-from src.data_processing.text_extractor import extract_text
 from src.utils.minhash_utils import HashFinder
+
 
 os.environ["RAY_RUNTIME_ENV_TEMPORARY_REFERENCE_EXPIRATION_S"] = "3600"
 
-init()
+init(ignore_reinit_error=True)
 
 def main():
     config = load_config()
-    output_dir = config["deduplication"]["output_dir"]
-    es_source = get_es_source_deduplication(config)
+    input_dir = config["dir"]["input_sample_dir"]
+    output_dir = config["dir"]["deduplication_dir"]
+    file_paths = [str(file) for file in Path(input_dir).glob("*.parquet")]
 
     (
-        read_datasource(
-            datasource=es_source,
-            concurrency=1000,
-            override_num_blocks=1000,
+        read_parquet_bulk(
+            paths=file_paths,
+            concurrency=100,
+            columns=["_id", "plaintext", "minhash"],
             ray_remote_args=dict(
                 num_cpus=0.01,
                 memory=2 * 1024**3,
                 max_retries=10, 
                 retry_exceptions=True,
-            ),
-        )
-        .map_batches(
-            fn=extract_text,
-            concurrency=1000,
-            num_cpus=0.01,
-            batch_format="pandas",
-            memory=2 * 1024**3,
+            )
         )
         .map_batches(
             fn=HashFinder(),
-            concurrency=1000,
+            concurrency=100,
             num_cpus=0.01,
             batch_format="pandas",
-            memory=5 * 1024**3,
+            memory=32 * 1024**3,
+            max_retries=10, 
+            retry_exceptions=True,
         )
         .write_parquet(
             path=output_dir,
+            concurrency=100,
             ray_remote_args=dict(
                 num_cpus=0.01,
                 memory=2 * 1024**3,
+                max_retries=10, 
+                retry_exceptions=True,
             )
         )
     )
