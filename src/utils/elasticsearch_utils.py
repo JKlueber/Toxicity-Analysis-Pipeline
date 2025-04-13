a703d7909b3021daa5a1734e2f8ef6cacdf962e1
Julian Klüber <julian.klueber@uni-jena.de>
2025-04-13 09:55:32 +0200

    filter out reblogs and media attachments + date range set to created_at

--- src/utils/elasticsearch_utils.py
+++ src/utils/elasticsearch_utils.py
@@ -33,57 +33,6 @@ def get_es_source(config):
                 "filter": [
                     {
                         "range": {
-                            "crawled_at": {
-                                "gte": config['date_range']['after'],
-                                "lte": config['date_range']['before'],
-                                "format": "date_hour_minute_second"
-                            }                                                                                                                            
-                        }
-                    },
-                    {
-                        "terms": {
-                            "crawled_from_instance": instances
-                        }
-                    },
-                    {
-                        "term": {
-                            "language": config['elasticsearch']['language']
-                        }
-                    }
-                ]
-            }
-        },
-        schema=schema([
-            field('_id', string()),
-            field("content", string()),
-            field("crawled_from_instance", string()),
-            field("instance", string()),
-            field("is_local", bool_()),
-            field("created_at", string()),
-            field("sensitive", bool_()),
-            field("spoiler_text", string()),
-        ]),
-    )
-
-def get_es_source_deduplication(config):
-    instances = read_instances_from_file(config)
-        
-    return ElasticsearchDatasource(
-        index=config['elasticsearch']['index'],
-        hosts=config['elasticsearch']['host'],
-        http_auth=(
-            config['elasticsearch']['user'], 
-            ELASTIC_PASSWORD,
-        ),
-        timeout=60*60*24, # 1 day
-        retry_on_timeout=True,
-        max_retries=20,
-        keep_alive="24h",
-        query={
-            "bool": {
-                "filter": [
-                    {
-                        "range": {
                             "created_at": {
                                 "gte": config['date_range']['after'],
                                 "lte": config['date_range']['before'],
@@ -101,11 +50,29 @@ def get_es_source_deduplication(config):
                             "language": config['elasticsearch']['language']
                         }
                     }
+                ],
+                "must_not": [
+                    {
+                        "exists": {
+                            "field": "reblog.id"
+                        }
+                    },
+                    {
+                        "exists": {
+                            "field": "media_attachments.id"
+                        }
+                    }
                 ]
             }
         },
         schema=schema([
+            field('_id', string()),
             field("content", string()),
-            field("_id", string()),
+            field("crawled_from_instance", string()),
+            field("instance", string()),
+            field("is_local", bool_()),
+            field("created_at", string()),
+            field("sensitive", bool_()),
+            field("spoiler_text", string()),
         ]),
     )
